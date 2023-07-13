a2783fc83cbbcb8ffed24aa9cd245fe84b3254c7
gerohaan <gero.delfin@gmail.com>
2023-07-12 23:01:40 -0400

    fix: Tables

--- database/migrations/20230713024744-create-subject.js
+++ database/migrations/20230713024744-create-subject.js
@@ -1,21 +1,19 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Notes', {
+    await queryInterface.createTable('Subjects', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      idSubject: {
-        type: Sequelize.INTEGER
-      },
-      value: {
+      name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
-      idStudent: {
-        type: Sequelize.INTEGER
+      description: {
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
@@ -28,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Notes');
+    await queryInterface.dropTable('Subjects');
   }
 };
